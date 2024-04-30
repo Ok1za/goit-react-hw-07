a6f6f1256cf8be7278ca4a0f24c8924b9a973df0
Ok1za <oksanaprescott@gmail.com>
2024-04-30 22:05:05 +0300

    add fix filtration

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,28 +1,34 @@
 import { useDispatch } from "react-redux";
 import styles from "./SearchBox.module.css";
 import { useState } from "react";
+import { setFilter } from "../../redux/contactsSlice";
 
 const SearchBox = () => {
     const dispatch = useDispatch();
-    const [filter, setFilter] = useState('');
+    const [filterValue, setFilterValue] = useState('');
 
     const handleFilterChange = (event) => {
-    const { value } = event.target;
-    setFilter(value);
-    dispatch(setFilter(value));
+        setFilterValue(event.target.value);
+    };
+
+    const handleFilterSubmit = () => {
+        dispatch(setFilter(filterValue));
     };
 
     return (
+        <div>
         <label className={styles.searchBoxLabel}>
             <span className={styles.searchBoxLabelText}>Find contacts by name</span>
             <input
                 className={styles.searchBoxInput}
                 type="text"
-                value={filter}
+                value={filterValue}
                 onChange={handleFilterChange}
                 placeholder="Search contacts"
             />
-        </label>
+            </label>
+            <button className={styles.searchButton} onClick={handleFilterSubmit}>Search</button>
+        </div>
     );
 };
 
